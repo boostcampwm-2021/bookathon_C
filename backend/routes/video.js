a5e981a9ce18795acfeef5894a94121800c3244c
J165_이창엽 <dlckdduq97@gmail.com>
2021-09-23 17:49:46 +0900

    [#4] DB 데이터 랜덤 선택
    DB에 있는 데이터 랜덤으로 선택하는 라우터 추가

--- backend/routes/video.js
+++ backend/routes/video.js
@@ -1,14 +1,18 @@
 const { postVideo, getVideo, updateVideo, deleteVideo } = require('../controllers/video')
-
+const randomChoice = require('../controllers/randomChoice');
 const express = require('express');
 const router = express.Router();
 
 router.post('/', postVideo);
 
+router.get('/random',randomChoice);
 router.get('/:videoId', getVideo);
 
 router.put('/:videoId', updateVideo);
 
 router.delete('/:videoId', deleteVideo);
+
+
+
 
 module.exports = router
