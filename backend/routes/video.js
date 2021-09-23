d59d3e9d6d6b68cd75569de1fbacdec06f0174cc
winters0727 <winters0727@gmail.com>
2021-09-24 01:53:25 +0900

    [#8] 동영상 업로드 기능 구현

--- backend/routes/video.js
+++ backend/routes/video.js
@@ -1,11 +1,12 @@
-const { postVideo, getVideo, updateVideo, deleteVideo } = require('../controllers/video')
+const { fileUpload, postVideo, getVideo, updateVideo, deleteVideo } = require('../controllers/video')
 const randomChoice = require('../controllers/randomChoice');
+
 const express = require('express');
 const router = express.Router();
 
-router.post('/', postVideo);
+router.post('/', fileUpload, postVideo)
 
-router.get('/random',randomChoice);
+router.get('/random', randomChoice);
 router.get('/:videoId', getVideo);
 
 router.put('/:videoId', updateVideo);
